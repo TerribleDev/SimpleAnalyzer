73a3afd61f5be8bc6b32efd518be04cb57b77e9d
Tommy Parnell <tparnell8@gmail.com>
2015-05-07 23:31:09 -0400

    ignore all

---
+++ Rakefile.rb
@@ -0,0 +1,8 @@
+# A rake task to build all packages in a folder
+ 
+  desc "Build the nuget packages"
+  task :package do
+    FileList["*.nuspec"].each do |spec|
+      sh "nuget pack #{spec} -Version %APPVEYOR_BUILD_VERSION%"
+    end
+  end
